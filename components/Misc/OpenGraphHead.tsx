9fbb3230915e9b8d7d61e5d776c70f378f9ae5d3
zafawniar <zafawniar@gmail.com>
2026-01-25 06:33:41 +0700

    chore: update og:image with branded text-based image

--- components/Misc/OpenGraphHead.tsx
+++ components/Misc/OpenGraphHead.tsx
@@ -5,15 +5,15 @@ const OpenGraphHead = () => (
     <meta name="description" content="My personal solace place on web-earth." />
     <meta property="og:title" content="Zafaa | Fullstack Dev & QA Engineer" />
     <meta property="og:site_name" content="Azaria Putri Fawnia" />
-    <meta property="og:url" content="https://kllawingco.netlify.app/" />
+    <meta property="og:url" content="https://azariafwn.vercel.app/" />
     <meta
       property="og:description"
-      content="Ohh you found me? Holla! I am Azaria Putri Fawnia. Or you could call me Zafaa. That works too . . . I am a Fullstack Dev & QA Engineer"
+      content="Ohh you found me? Holla! I am Azaria Putri Fawnia. I am a Fullstack Dev & QA Engineer"
     />
     <meta property="og:type" content="profile" />
     <meta
       property="og:image"
-      content="https://kllawingco.netlify.app/KL_avatar.png"
+      content="https://azariafwn.vercel.app/zaf.png"
     ></meta>
   </Head>
 )
